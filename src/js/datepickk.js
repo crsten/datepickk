e154ed7841de715179f5619a26f9df23d4705e99
Đàm <14832060+tieume@users.noreply.github.com>
2019-05-31 13:49:58 +0700

    Additional language for japanese

--- src/js/datepickk.js
+++ src/js/datepickk.js
@@ -40,7 +40,12 @@ function Datepickk(args){
 			monthNames:['Januar','Februar','März','April','Mai','Juni','Juli','August','September','Oktober','November','Dezember'],
 			dayNames:['So','Mo','Di','Mi','Do','Fr','Sa'],
 			weekStart:1
-		}
+		},
+		ja: {
+			monthNames:['一月','二月','三月','四月','五月','六月','七月','八月','九月','十月','十一月','十二月'],
+			dayNames:['日','月','火','水','木','金','土'],
+			weekStart:0
+		},
 	};
 
 	/*Language aliases*/
